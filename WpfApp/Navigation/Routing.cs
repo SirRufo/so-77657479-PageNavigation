6501ff8ccb845694709ef5c080d735c42b437690
Oliver Münzberg <oliver.muenzberg@gmail.de>
2023-12-14 10:18:42 +0100

    clean up code

--- WpfApp/Navigation/Routing.cs
+++ WpfApp/Navigation/Routing.cs
@@ -2,22 +2,7 @@ namespace WpfApp.Navigation;
 
 public static class Routing
 {
-    public static string GetRoute( FrameworkElement obj )
-    {
-        return (string)obj.GetValue( RouteProperty );
-    }
-
-    public static void SetRoute( FrameworkElement obj, string value )
-    {
-        obj.SetValue( RouteProperty, value );
-    }
-
-    // Using a DependencyProperty as the backing store for Route.  This enables animation, styling, binding, etc...
-    public static readonly DependencyProperty RouteProperty =
-        DependencyProperty.RegisterAttached( "Route", typeof( string ), typeof( Routing ), new PropertyMetadata( string.Empty ) );
-
     private static readonly Dictionary<string, Type> _registeredRoutes = new Dictionary<string, Type>();
-
 
     public static FrameworkElement GetOrCreateContent( string route, IServiceProvider serviceProvider )
     {
